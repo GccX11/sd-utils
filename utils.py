ffd1cedfe44c94459947f1bba66df106fdde6b4a
gccx11 <gccx11>
2022-12-15 23:14:03 -0500

    Implemented the GAN from the tutorial. Played with the latents between dark and lite bg images. UMAP shows clear structure, and when projecting onto the dark-lite vector, we can clearly classify the differences between the known images. TODO: I guess I would need to hold out a test set to make sure it still works on those data. That would be the next test, for sure.

--- utils.py
+++ utils.py
@@ -140,15 +140,6 @@ def get_text_embeds(prompt):
     with torch.no_grad():
         text_embeddings = text_encoder(text_input.input_ids.to(device))[0]
 
-    # Do the same for unconditional embeddings
-    uncond_input = tokenizer(
-        [''] * len(prompt), padding='max_length',
-        max_length=tokenizer.model_max_length, return_tensors='pt')
-    with torch.no_grad():
-        uncond_embeddings = text_encoder(uncond_input.input_ids.to(device))[0]
-
-    # Cat for final embeddings
-    text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
     return text_embeddings
 
 
@@ -228,7 +219,7 @@ def produce_latents(text_embeddings, height=512, width=512,
     if start_latent is not None:
         latents = start_latent
     else:
-        start_latent = torch.randn((text_embeddings.shape[0] // 2, unet.in_channels, \
+        start_latent = torch.randn((text_embeddings.shape[0], unet.in_channels, \
                                     height // 8, width // 8))
         latents = start_latent
     
@@ -248,7 +239,7 @@ def produce_latents(text_embeddings, height=512, width=512,
     with autocast('cuda'):
         for i, t in tqdm(enumerate(scheduler.timesteps[start_step:])):
             # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
-            latent_model_input = torch.cat([latents] * 2)
+            latent_model_input = latents
             sigma = scheduler.sigmas[i]
             latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
             
@@ -257,8 +248,8 @@ def produce_latents(text_embeddings, height=512, width=512,
                 noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)['sample']
 
             # perform guidance
-            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+            #noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            #noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
             # compute the previous noisy sample x_t -> x_t-1
             latents = scheduler.step(noise_pred, t, latents)['prev_sample']
